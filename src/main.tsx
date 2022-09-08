adc824c47c119e03c1681bfdc2b7694ba2915c9a
Carlos Manuel Fraile Gomez <carlosfrailegomez93@gmail.com>
2022-09-08 10:44:36 +0200

    235

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,21 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { Routes , Route, BrowserRouter } from 'react-router-dom';
+import { AuthRoutes } from './auth/routes/AuthRoutes';
+import { JournalRoutes } from './journal/routes/JournalRoutes';
 import './main.sass'
 
 const Japp = () => {
   return(
-      <h1>Cascarón react - MaterialUi</h1>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/auth/*' element={<AuthRoutes/>} />
+        <Route path='/*' element={<JournalRoutes/>} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(<React.StrictMode><Japp></Japp></React.StrictMode>);
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+<React.StrictMode><Japp></Japp></React.StrictMode>
+);
