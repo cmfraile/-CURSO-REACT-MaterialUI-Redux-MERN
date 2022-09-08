3019e7ebb17781516cd8985417ff7e542ea6d464
Carlos Manuel Fraile Gomez <carlosfrailegomez93@gmail.com>
2022-09-08 12:46:14 +0200

    239

--- src/auth/pages/LoginPage.tsx
+++ src/auth/pages/LoginPage.tsx
@@ -1,5 +1,33 @@
+import { Grid , Typography , TextField } from '@mui/material'
+
 export const LoginPage = () => {
     return(
-        <h1>Login Page</h1>
+        <Grid   container
+                spacing={0}
+                direction='column'
+                alignItems='center'
+                justifyContent='center'
+                sx={{minHeight:'100vh',backgroundColor:'primary.main',pt:4}}
+        >
+
+        <Grid   item
+                sx={{backgroundColor:'white',padding:3,borderRadius:3}}
+        >
+            <Typography variant='h5' sx={{mb:1}}> Login </Typography>
+            <Grid item>
+                <TextField  label='Correo'
+                            type='email'
+                            sx={{margin:'10px 0px'}}
+                            fullWidth
+            /></Grid>
+            <Grid item>
+                <TextField  label='pass'
+                            type='password'
+                            sx={{margin:'10px 0px'}}
+                            fullWidth
+            /></Grid>
+        </Grid>
+
+        </Grid>
     )
 }
