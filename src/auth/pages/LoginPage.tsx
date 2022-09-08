8000d6730663b6c88c24e7d64ba5db0841db1e29
Carlos Manuel Fraile Gomez <carlosfrailegomez93@gmail.com>
2022-09-08 13:09:59 +0200

    239

--- src/auth/pages/LoginPage.tsx
+++ src/auth/pages/LoginPage.tsx
@@ -1,4 +1,6 @@
-import { Grid , Typography , TextField } from '@mui/material'
+import { Google } from '@mui/icons-material';
+import { Link as RrdLink } from 'react-router-dom';
+import { Grid , Typography , TextField , Button , Link } from '@mui/material'
 
 export const LoginPage = () => {
     return(
@@ -26,7 +28,22 @@ export const LoginPage = () => {
                             sx={{margin:'10px 0px'}}
                             fullWidth
             /></Grid>
+
+            <Grid container spacing={2} sx={{mb:2,mt:1}}>
+                <Grid item xs={12} sm={6}><Button fullWidth variant='contained'>Login</Button></Grid>
+                <Grid item xs={12} sm={6}><Button fullWidth variant='contained'><Google sx={{margin:'0px 5px'}}/>Google</Button></Grid>
+            </Grid>
+            
+            <Grid container direction='row' justifyContent='end'>
+                <Link component={ RrdLink } color='inherit' to='/auth/register'>
+                    Crear una cuneta
+                </Link>
+
+            </Grid>
+
         </Grid>
+
+        
 
         </Grid>
     )
